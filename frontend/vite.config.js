6d52b7834b20bdee3da52dc44a88df8b8b8c6521
Alok Raj <13000122091.tmsl@ticollege.org>
2025-11-30 04:05:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    // This forces all libraries to use YOUR version of React
-    dedupe: ['react', 'react-dom'],
-  },
 })
