4e0e02dc1ec6412ffe48b7b984b988df84bcbd14
Alok Raj <13000122091.tmsl@ticollege.org>
2026-07-18 16:15:17 +0530

    Revert "Update RecommendationCard.jsx"
    This reverts commit 5dc18ba385dcc8d10b24f3653baacfb7f0ab90e6.

--- frontend/src/components/recommendations/RecommendationCard.jsx
+++ frontend/src/components/recommendations/RecommendationCard.jsx
@@ -53,7 +53,6 @@ export default function RecommendationCard({ recommendation }) {
 
     trackInteraction(_id, action, {
       source: "recommendation",
-      title: book.title,
     });
 
     toast.success(wishlisted ? "Removed from wishlist" : "Added to wishlist");
@@ -95,7 +94,6 @@ export default function RecommendationCard({ recommendation }) {
       toast.success(`${title} added to cart`);
       await trackInteraction(_id, "ADD_CART", {
         source: "recommendation",
-        title: book.title,
       });
     } catch (err) {
       console.error(err);
@@ -168,7 +166,6 @@ ${stock === 0 ? "bg-gray-200 cursor-not-allowed" : "bg-white hover:scale-110"}`}
         onClick={() =>
           trackInteraction(_id, "CLICK", {
             source: "recommendation",
-            title: book.title,
           })
         }
       >
